9f0b5550219fcf720fba45756b546fd18126cece
sorakoro <sorakoro9211@gmail.com>
2022-10-19 02:04:02 +0900

    set the routes constraints for the api

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  namespace :api, defaults: { format: :json }  do
+    # We are going to list our resources here
+  end
 end
