1e973c2161e06035b1f2db9c965332bd0ed6c94c
sorakoro <sorakoro9211@gmail.com>
2022-10-20 00:56:45 +0900

    generate product model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_18_171430) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_19_155033) do
+  create_table "products", force: :cascade do |t|
+    t.string "title"
+    t.decimal "price"
+    t.boolean "published"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_products_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
@@ -18,4 +28,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_18_171430) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "products", "users"
 end
