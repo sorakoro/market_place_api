c58c0b85bba446364cadaf5c09a07d2a546e107e
sorakoro <sorakoro9211@gmail.com>
2022-10-19 16:47:56 +0900

    adds the user create endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json }  do
     namespace :v1 do
       # We are going to list our resources here
-      resources :users, only: [:show]
+      resources :users, only: %i[show create]
     end
   end
 end
